b23eea53be966f38d8be3b5505bc9b12e6b9ee85
minhng97 <minh.nguyentrong97@gmail.com>
2020-01-16 23:29:27 +0700

    First commit

---
+++ src/components/TrafficLight.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './TrafficLight.css';
+
+const RED = 0;
+const ORANGE = 1;
+const GREEN = 2;
+
+class TrafficLight extends Component {
+    constructor(props) {
+        super(props);
+        this.currentColor = RED;
+    }
+
+    getNextColor(color) {
+        switch(color) {
+            case RED:
+                return ORANGE;
+            case ORANGE:
+                return GREEN;
+            default:
+                return RED;
+        }
+    }
+
+    render() {
+        return <div className="TrafficLight">
+            <div className="bulb red" /> 
+            <div className="bulb orange" /> 
+            <div className="bulb green" /> 
+        </div>;
+    }
+}    
+
+export default TrafficLight;
