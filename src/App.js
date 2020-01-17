28a53cab7f87615ac595d1d7627c1286c8c62814
minhng97 <minh.nguyentrong97@gmail.com>
2020-01-17 11:54:59 +0700

    constructor moved from trafficLight to here

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import TrafficLight from './components/TrafficLight'
 
-function App() {
-  return (
-    <div className="App">
-        <TrafficLight />
-    </div>
-  );
+const RED = 0;
+const ORANGE = 1;
+const GREEN = 2;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        currentColor: GREEN
+    }
+
+    setInterval(() => {
+        console.log(this.state.currentColor)
+        this.setState({
+            currentColor: this.getNextColor(this.state.currentColor)
+        });
+    }, 1000)
+    
+}
+
+getNextColor(color) {
+    switch(color) {
+        case RED:
+            return ORANGE;
+        case ORANGE:
+            return GREEN;
+        default:
+            return RED;
+    }
+}
+
+  render() {
+    const {currentColor} = this.state;
+      return (
+        <div className="App">
+            <TrafficLight currentColor={currentColor}/>
+        </div>
+       );
+  }
+
 }
 
 export default App;
